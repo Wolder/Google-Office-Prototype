9b667c30701992e188503006ef1391d221bb5371
mortenabra <sezure@gmail.com>
2020-11-29 23:00:44 +0100

    Newest controller

--- app/src/main/java/com/example/genterprise/Service/DeviceFetchingService.java
+++ app/src/main/java/com/example/genterprise/Service/DeviceFetchingService.java
@@ -2,41 +2,25 @@ package com.example.genterprise.Service;
 
 import android.content.Context;
 import android.util.Log;
-
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-import com.github.nkzawa.emitter.Emitter;
-import com.github.nkzawa.socketio.client.IO;
-import com.github.nkzawa.socketio.client.Socket;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.FileNotFoundException;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URISyntaxException;
-import java.net.URL;
+import java.io.InputStreamReader;
+import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
 
 public class DeviceFetchingService {
 
     private static final String TAG = "DeviceFetchingImpl";
-    private String ip = "http://10.0.2.2/";
+    private String ip = "10.0.2.2";
     private int port = 8080;
     private List<String> jsonResponse = new ArrayList<>();
-    private Socket socket;
-    private RequestQueue requestQueue;
     private Context context;
+    private Socket socket;
+    private Thread t;
+
 
     public DeviceFetchingService() {
         this.ip = ip;
@@ -44,74 +28,47 @@ public class DeviceFetchingService {
         this.context = context;
     }
 
-    public void connect() {
-            IO.Options opts = new IO.Options();
-            opts.port = 8080;
-            try {
-                socket = IO.socket(ip, opts);
-            } catch (URISyntaxException e) {
-                e.printStackTrace();
-            }
-    }
-
     public String getIP() {
         return this.ip;
     }
 
-    public Integer getPort() {
+    public int getPort() {
         return this.port;
     }
 
-    public void httpGET(){
-        boolean dataSent = false;
-        socket.connect();
-        Log.d(TAG, "httpGET: Connecting to server...");
-        if(socket.connected()){
-            Log.d(TAG, "httpGET: Connected...");
-            emitData();
-            Log.d(TAG, "httpGET: Data emitted...");
-            dataSent = true;
-        }
-        if (dataSent){
-            socket.on("new message", onResponse);
-            socket.connect();
-        }
-    }
 
-    private Emitter.Listener onResponse = new Emitter.Listener() {
-        @Override
-        public void call(Object... args) {
-            new Runnable() {
-                @Override
-                public void run() {
-                    JSONObject data = (JSONObject) args[0];
-                    String usr;
-                    String msg;
-                    try {
-                        usr = data.getString("name");
-                        msg = data.getString("message");
-                    } catch (JSONException e){
-                        return;
-                    }
-                    Log.d(TAG, "Results from server: " + usr + " : " + msg);
+    public void connect() {
+        t = new Thread("Data Fetching"){
+            @Override
+            public void run() {
+                try {
+                    socket = new Socket(ip, port);
+                    DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+                    dos.writeUTF("GET:JSON:");
+
+                    //read input stream
+                    DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+                    InputStreamReader inputStreamReader = new InputStreamReader(dataInputStream);
+                    BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+
+                    //print the input to the application screen
+                    Log.d(TAG, "Response!" + bufferedReader.toString());
+                    String resp;
+                    //while ((resp = br.readLine()) != null) {
+                    //    Log.d(TAG, resp);
+                    //}
+
+                    dataInputStream.close();
+                    socket.close();
+
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
-            };
-        }
-    };
-
-    private void emitData(){
-        String msg = "GET:JSON:";
-        socket.emit("new message", msg);
+            }
+        };
+        t.start();
     }
-
-
-
-/*    public void initWorking(){
-        executor = Executors.newCachedThreadPool();
-        for (int i = 0; i < 20; i++) {
-            executor.execute(new DeviceFetchingService());
-        }
-    }*/
 
 
 }
