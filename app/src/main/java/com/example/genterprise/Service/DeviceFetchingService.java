3beaffe8a6a67e7d0c2effb7f0fb65c21a21f6e0
mortenabra <sezure@gmail.com>
2020-11-29 15:54:49 +0100

    For Nick hihihih

--- app/src/main/java/com/example/genterprise/Service/DeviceFetchingService.java
+++ app/src/main/java/com/example/genterprise/Service/DeviceFetchingService.java
@@ -3,6 +3,12 @@ package com.example.genterprise.Service;
 import android.content.Context;
 import android.util.Log;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.github.nkzawa.emitter.Emitter;
 import com.github.nkzawa.socketio.client.IO;
 import com.github.nkzawa.socketio.client.Socket;
@@ -13,7 +19,10 @@ import org.json.JSONObject;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Executor;
@@ -22,24 +31,27 @@ import java.util.concurrent.Executors;
 public class DeviceFetchingService {
 
     private static final String TAG = "DeviceFetchingImpl";
-    private String ip = "http://10.0.2.2:5000/";
-    private int port = 5000;
+    private String ip = "http://10.0.2.2/";
+    private int port = 8080;
     private List<String> jsonResponse = new ArrayList<>();
     private Socket socket;
+    private RequestQueue requestQueue;
+    private Context context;
 
     public DeviceFetchingService() {
         this.ip = ip;
         this.port = port;
+        this.context = context;
     }
 
-    public void connect(){
-        try {
-            socket = IO.socket(ip);
-            httpGET();
-        } catch (URISyntaxException e) {
-            Log.d(TAG, "connect: Error on connection!");
-            e.printStackTrace();
-        }
+    public void connect() {
+            IO.Options opts = new IO.Options();
+            opts.port = 8080;
+            try {
+                socket = IO.socket(ip, opts);
+            } catch (URISyntaxException e) {
+                e.printStackTrace();
+            }
     }
 
     public String getIP() {
@@ -91,6 +103,8 @@ public class DeviceFetchingService {
         String msg = "GET:JSON:";
         socket.emit("new message", msg);
     }
+
+
 
 /*    public void initWorking(){
         executor = Executors.newCachedThreadPool();
